8e435c10fd9a8c8adf7af96ec1d6b3218846e663
KELERPPC\stefa <stefankeler96@gmail.com>
2017-05-04 20:13:09 +0200

    Testovi v1

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,35 +1,64 @@
+import sun.net.www.content.text.plain;
+
 public class TennisGame 
 {	
 	private int player1Points;
 	private int player2Points;
 	private boolean gameEnded;
-	
+
 	public TennisGame() 
 	{
 		// TO BE IMPLEMENTED
+		player1Points = 0;
+		player2Points = 0;
+		gameEnded = false;
 	}
 
 	public void player1Scored() throws TennisGameException 
 	{
-		// TO BE IMPLEMENTED
+		if(gameEnded){
+			throw new TennisGameException();
+		}else{
+			player1Points++;
+			checkGameEnded();
+		}
+
 	}
 
 	public void player2Scored() throws TennisGameException 
 	{
-		// TO BE IMPLEMENTED
+		if(gameEnded){
+			throw new TennisGameException();
+		}else{
+			player2Points++;
+			checkGameEnded();
+		}
 	}
-	
+
 	private void checkGameEnded()
 	{ 
-		// TO BE IMPLEMENTED
+		if(Math.abs(player1Points - player2Points) == 2 && (player1Points > 3 || player2Points > 3)){
+			gameEnded = true;
+		}
 	}
 
 	private String getScore(int points) 
 	{
-		// TO BE IMPLEMENTED
+		if(points == 0){
+			return "0";
+		}else if(points == 1){
+			return "15";
+		}else if(points == 2){
+			return "30";
+		}else if(points == 3){
+			return "40";
+		}else {
+			
+		}
+		
 		return "";
 	}
-	
+
 	public String getScore() 
 	{
 		// Scores format: "player1Score - player2Score"
@@ -48,6 +77,19 @@ public class TennisGame
 		// "game player2"
 
 		// TO BE IMPLEMENTED
+		if(player1Points < 3 || player2Points < 3){
+			return getScore(player1Points) + " - " + getScore(player2Points);
+		}else if(player1Points == player2Points + 1){
+			return "advantage player1";
+		}else if(player1Points == player2Points + 2){
+			return "game player1";
+		}else if(player1Points + 1 == player2Points){
+			return "advantage player2";
+		}else if(player1Points + 2 == player2Points){
+			return "game player2";
+		}else if (player1Points == player2Points){
+			return "deuce";
+		}
 		return "";
 	}
 }
