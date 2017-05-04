868576533f86f03a30025bfe9cced3c46e9db695
somename <someemail>
2017-05-04 20:45:46 +0200

    besouro automatic message

--- tests/NumericScores.java
+++ tests/NumericScores.java
@@ -244,8 +244,6 @@ public class NumericScores {
 		TennisGame game = new TennisGame();
 		game.player1Scored();
 		game.player1Scored();
-		game.player2Scored();
-		game.player2Scored();
 		game.player1Scored();
 		game.player1Scored();
 		String score = game.getScore();
