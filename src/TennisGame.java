94692ea7af7f0a1a08c5ee70ada3a8c6c748bca2
somename <someemail>
2017-05-05 20:38:04 +0200

    besouro automatic message

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -51,17 +51,28 @@ public class TennisGame
 	
 	public String getScore() 
 	{
-		if(player1Points == 0 && player2Points == 0)
+		if(player1Points < 3 && player2Points < 3)
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if(player1Points == 1 && player2Points == 1)
+		else if(player1Points > 3 && player2Points > 3 && player1Points == player2Points)
+			return getScore(4);
+		else if((player1Points ==1 && player2Points == 0 ) || (player1Points == 0 && player2Points == 1 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if(player1Points == 2 && player2Points == 2)
+		else if((player1Points == 2  && player2Points == 0 ) || (player1Points == 0 && player2Points == 2 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if(player1Points == 4 && player2Points == 4)
+		else if((player1Points == 3  && player2Points == 0 ) || (player1Points == 0 && player2Points == 3 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if(player1Points == 1 && player2Points == 0)
+		else if((player1Points == 2  && player2Points == 1 ) || (player1Points == 1 && player2Points == 2 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if(player1Points == 2)
+		else if((player1Points == 3  && player2Points == 2 ) || (player1Points == 1 && player2Points == 2 ))
+			return getScore(player1Points) + " - " + getScore(player2Points);
+		else if(player1Points >3 && player1Points-player2Points ==1)
+			return "advantage player1";
+		else if(player2Points >3 && player2Points-player1Points ==1)
+			return "advantage player2";
+		else if(player1Points > 3 && player1Points-player2Points == 2)
+			return "game player1";
+		else if(player2Points > 3 && player2Points-player1Points == 2)
+			return "game player2";
 		// Scores format: "player1Score - player2Score"
 		// "0 - 0"
 		// "15 - 15"
