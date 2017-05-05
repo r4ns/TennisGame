c808bc5be6f8f4d77e5c06ab64577d3f9ebac905
it25g2015 <it25g2015@ws9.itc03.edu>
2017-05-05 13:03:55 +0200

    TennisGame

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -4,30 +4,53 @@ public class TennisGame
 	private int player2Points;
 	private boolean gameEnded;
 	
-	public TennisGame() 
+	public TennisGame(){
+		
+	}
+	
+	public TennisGame(int player1, int player2, boolean endGame) 
 	{
-		// TO BE IMPLEMENTED
+		player1 = this.player1Points;
+		player2 = this.player2Points;
+		endGame = this.gameEnded;
 	}
 
 	public void player1Scored() throws TennisGameException 
 	{
-		// TO BE IMPLEMENTED
+		player1Points++;
 	}
 
 	public void player2Scored() throws TennisGameException 
 	{
-		// TO BE IMPLEMENTED
+		player2Points++;
 	}
 	
 	private void checkGameEnded()
 	{ 
-		// TO BE IMPLEMENTED
+		if((player1Points == 4) && (player2Points < 3)){
+			gameEnded = true;
+		}else if((player1Points == 4) && (player2Points < 3)){
+			gameEnded = true;
+		}else if((player1Points == 4) && (player2Points == 4) && (player1Points > player2Points)){
+			gameEnded = true;
+		}else if((player1Points == 4) && (player2Points == 4) && (player2Points > player1Points)){
+			gameEnded = true;
+		}else
+			gameEnded = false;
 	}
 
 	private String getScore(int points) 
 	{
-		// TO BE IMPLEMENTED
-		return "";
+		if(points == 1)
+		return "15";
+		else if(points == 2)
+			return "30";
+		else if(points == 3)
+			return "40";
+		else if(points == 0)
+			return "0";
+		else
+			return "Greska!";
 	}
 	
 	public String getScore() 
@@ -48,6 +71,23 @@ public class TennisGame
 		// "game player2"
 
 		// TO BE IMPLEMENTED
-		return "";
+		if((player1Points == 0) && (player2Points == 0))
+			return "0 - 0";
+		if((player1Points == 15) && (player2Points == 15))
+			return "15 - 15";
+		else if((player1Points == 3) &&(player2Points == 3))
+		return "duece";
+		else if((player1Points == 4) && (player2Points == 3) && (player1Points > player2Points))
+			return "advantage player1";
+		else if((player1Points == 3) && (player2Points == 4) && (player2Points > player1Points))
+			return "advantage player2";
+		else if((player1Points == 4) && (player2Points < 3))
+			return "game player1";
+		else if((player2Points == 4) && (player1Points < 3))
+			return "game player2";
+		else
+			return getScore(player1Points) + " - " + getScore(player2Points);
+		
+		
 	}
 }
