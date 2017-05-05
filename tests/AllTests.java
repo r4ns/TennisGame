d686c36da239f15ff5d6be2e5d45f636e4c862b7
somename <someemail>
2017-05-05 12:53:31 +0200

    besouro automatic message

--- tests/AllTests.java
+++ tests/AllTests.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -5,5 +8,19 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ NumericScores.class})
 public class AllTests {
-
+	@Test
+	public void test1 throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();		
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored twice", "40 - 40", score);	
+	}
 }
