e1c78fc68325ee58d08112926a71cfad30f6c9eb
somename <someemail>
2017-05-05 20:52:44 +0200

    besouro automatic message

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -68,9 +68,11 @@ public class TennisGame
 			return getScore(player1Points) + " - " + getScore(player2Points);
 		else if((player1Points == 2  && player2Points == 1 ) || (player1Points == 1 && player2Points == 2 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
-		else if((player1Points == 3  && player2Points == 2 ) || (player1Points == 1 && player2Points == 2 ))
+		else if((player1Points == 3  && player2Points == 2 ) || (player1Points == 2 && player2Points == 3 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
 		else if((player1Points == 3  && player2Points == 1 ) || (player1Points == 1 && player2Points == 3 ))
+			return getScore(player1Points) + " - " + getScore(player2Points);
+		else if((player1Points == 3  && player2Points == 2 ) || (player1Points == 1 && player2Points == 3 ))
 			return getScore(player1Points) + " - " + getScore(player2Points);
 		else if(player1Points >3 && player1Points-player2Points ==1)
 			return "advantage player1";
