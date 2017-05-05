8bb3ae8bb0b544e5db7aec26fb83024a22d64490
somename <someemail>
2017-05-05 13:02:48 +0200

    besouro automatic message

--- tests/AllTests.java
+++ tests/AllTests.java
@@ -9,21 +9,4 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ NumericScores.class})
 public class AllTests {
 	
-	@Test
-	public void test_game_igrac2() throws TennisGameException {
-		//Arrange
-		TennisGame game = new TennisGame();		
-		game.player1Scored();
-		game.player1Scored();
-		game.player1Scored();
-		game.player2Scored();
-		game.player2Scored();
-		game.player2Scored();
-		game.player2Scored();
-		game.player2Scored();
-		//Act
-		String score = game.getScore() ;
-		// Assert
-		assertEquals("Incorrect score when player1 scored twice", "game igrac2", score);	
-	}
 }
