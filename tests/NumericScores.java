1025f8d23c7af814f9ef5ed248a609c81984dc42
somename <someemail>
2017-05-05 13:03:21 +0200

    besouro automatic message

--- tests/NumericScores.java
+++ tests/NumericScores.java
@@ -245,4 +245,22 @@ public class NumericScores {
 		// Assert
 		assertEquals("Incorrect score when player1 scored twice", "game igrac2", score);	
 	}
+	
+	@Test
+	public void test_game_igrac2() throws TennisGameException {
+		//Arrange
+		TennisGame game = new TennisGame();		
+		game.player1Scored();
+		game.player1Scored();
+		game.player1Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player2Scored();
+		game.player1Scored();
+		//Act
+		String score = game.getScore() ;
+		// Assert
+		assertEquals("Incorrect score when player1 scored twice", "deuce", score);	
+	}
 }
