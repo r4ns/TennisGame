99415131941c53d0d426d328f8109099a87e8f16
somename <someemail>
2017-05-05 12:57:42 +0200

    besouro automatic message

--- tests/NumericScores.java
+++ tests/NumericScores.java
@@ -239,6 +239,7 @@ public class NumericScores {
 		game.player2Scored();
 		game.player2Scored();
 		game.player2Scored();
+		game.player2Scored();
 		//Act
 		String score = game.getScore() ;
 		// Assert
